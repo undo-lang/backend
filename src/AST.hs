b386782d6b9d168a09e009004abc63b050c1cfb9
vendethiel <vendethiel@hotmail.fr>
2019-03-29 15:14:07 +0100

    WIP Json parsing

--- src/AST.hs
+++ src/AST.hs
@@ -1,32 +1,61 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module AST
 --  (
 --    ..
 --  )
   where
 
-data Literal
-  = LitStr String
-  | LitNum Integer
+import Data.Aeson --(FromJSON(..), (.:), withObject, Object, Object(..))
 
 newtype ModuleName = ModuleName [String]
 data Name
   = QualifiedName ModuleName String
   | UnqualifiedName String
 
+instance FromJSON Name where
+  parseJSON = withObject "name" $ \o -> do
+    type_ <- o .: "type"
+    case type_ of
+      "Unqualified" -> UnqualifiedName <$> o .: "name"
+      "Qualified"   -> fail "Unimplemented: qualified names"
+      _            -> fail $ "Unhandled name type: " ++ type_
+
 data Expr
-  = LiteralExpr Literal
+  = LitStr String
+  | LitNum Int
   | CallExpr Expr [Expr]
   | LoopExpr Expr Block
   | ConditionalExpr Expr Block Block
 
-data Import = Import ModuleName
-data Var = String -- maybe newtype UnqualifiedName?
+instance FromJSON Expr where
+  parseJSON = withObject "expr" $ \o -> do
+    type_ <- o .: "type"
+    case type_ of
+      "String" -> LitStr <$> o .: "value"
+      "Num"    -> LitNum <$> o .: "value"
+      "Call"   -> CallExpr <$> o .: "fn" <*> o .: "argument"
+      _        -> fail $ "Unknown expr type: " ++ type_
 
-newtype Parameter = Parameter String -- ^
+newtype Parameter = Parameter String -- newtype the String?
 newtype ParameterList = ParameterList [Parameter]
-data Fn = Fn String ParameterList -- ^
 
-data Decl = ImportDecl Import | VarDecl Var | FnDecl Fn
+data Decl
+  = Import ModuleName
+  | Var String
+  | Fn String ParameterList Block
+--instance FromJSON Decl where
+--  parseJSON = withObject "decl" $ \o -> do
+--    type_ <- o .: "type"
+--    case type_ of
+--      "Import" -> Import <$> o .: "value" -- TODO check
+--      "Var"    -> Var <$> o .: "name"     -- TODO check
+--      "Fn"     -> Fn <$> o .: "fn" <*> o .: "parameter" <*> o .: "body" -- TODO check
 
 data Line = LineExpr Expr | LineDecl Decl
+--instance FromJSON Line where
+--  parseJSON = withObject "line" $ \o -> asum [
+--      LineExpr <$> parseJSON (Object o)
+--      LineDecl <$> parseJSON (Object o)
+--    ]
 data Block = Block [Line] -- todo takewhile isDecl + error if contains LineDecl in _2
