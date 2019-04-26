fd9d5e2ad20b9835df7a9435bf929566ee577b2a
vendethiel <vendethiel@hotmail.fr>
2019-04-26 14:48:31 +0200

    Index the AST over name resolution stage

--- src/AST.hs
+++ src/AST.hs
@@ -1,13 +1,14 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
 -- LANGUAGE BlockArguments
 
-module AST
---  (
---    ..
---  )
-  where
+module AST where
 
 import GHC.Generics
 import Control.Monad (guard)
@@ -16,29 +17,32 @@ import Data.Aeson --(FromJSON(..), (.:), withObject, Object, Object(..))
 
 newtype ModuleName = ModuleName [String]
   deriving (Generic, FromJSON, Show)
-data Name
-  = QualifiedName ModuleName String
-  | UnqualifiedName String
-  deriving (Show)
 
-instance FromJSON Name where
+data NameStage = U | R
+data Name :: NameStage -> * where
+  Unresolved :: String -> Name 'U
+  Variable   :: String -> Name 'R
+  -- TODO other names
+deriving instance Show (Name s)
+
+instance FromJSON (Name 'U) where
   parseJSON = withObject "name" $ \o -> do
     type_ <- o .: "type"
     case type_ of
-      "Unqualified" -> UnqualifiedName <$> o .: "name"
+      "Unqualified" -> Unresolved <$> o .: "name"
       "Qualified"   -> fail "Unimplemented: qualified names"
       _             -> fail $ "Unhandled name type: " ++ type_
 
-data Expr
+data Expr s
   = LitStr String
   | LitNum Int
-  | CallExpr Expr [Expr]
-  | LoopExpr Expr Block
-  | ConditionalExpr Expr Block Block
-  | NameExpr Name
+  | CallExpr (Expr s) [Expr s]
+  | LoopExpr (Expr s) (Block s)
+  | ConditionalExpr (Expr s) (Block s) (Block s)
+  | NameExpr (Name s)
   deriving (Show)
 
-instance FromJSON Expr where
+instance FromJSON (Expr 'U) where
   parseJSON = withObject "expr" $ \o -> asum [
     NameExpr <$> parseJSON (Object o),
     do
@@ -50,40 +54,40 @@ instance FromJSON Expr where
         _        -> fail $ "Unknown expr type: " ++ type_
     ]
 
-newtype Parameter = Parameter String -- newtype the String?
+newtype Parameter = Parameter String
   deriving (Generic, FromJSON, Show)
 
 newtype ParameterList = ParameterList [Parameter]
   deriving (Generic, FromJSON, Show)
 
-data Decl
+data Decl s
   = Import ModuleName
   | Var String
-  | Fn String ParameterList Block
+  | Fn String ParameterList (Block s)
   deriving (Show)
 
-instance FromJSON Decl where
+instance FromJSON (Decl 'U) where
   parseJSON = withObject "decl" $ \o -> do
     type_ <- o .: "type"
     case type_ of
-      "Import" -> Import <$> o .: "value" -- TODO check
-      "Var"    -> Var <$> o .: "name"     -- TODO check
+      "Import" -> Import <$> o .: "value"
+      "Var"    -> Var <$> o .: "name"
       "Fn"     -> Fn <$> o .: "fn" <*> o .: "parameter" <*> o .: "block"
       _        -> fail $ "Unknown decl type: " ++ type_
 
-data Line = LineExpr Expr | LineDecl Decl
+data Line s = LineExpr (Expr s) | LineDecl (Decl s)
   deriving (Show)
 
-instance FromJSON Line where
+instance FromJSON (Line 'U) where
   parseJSON = withObject "line" $ \o -> asum [
       LineExpr <$> parseJSON (Object o),
       LineDecl <$> parseJSON (Object o)
     ]
 
-newtype Block = Block [Line]
+newtype Block s = Block [Line s]
   deriving (Show)
 
-instance FromJSON Block where
+instance FromJSON (Block 'U) where
   parseJSON = withObject "block" $ \o -> do
     type_ <- o .: "type"
     guard ((type_ :: String) == "Block")
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE DataKinds #-}
 module Lib
   ( compile
   ) where
 
 import qualified Data.ByteString.Lazy as BS
-import Data.Aeson (eitherDecode)
 import Control.Lens (_Left, _Right, set, over)
 import Control.Monad (foldM)
 import Data.Functor (($>))
+import Data.List (mapAccumL)
+
+import Data.Aeson (eitherDecode)
+
 import AST
 
 compile :: BS.ByteString -> IO ()
 compile contents = putStrLn $ show $ do
   block <- readBlock contents
-  checkUseBeforeDeclare block []
+  checkUseBeforeDeclare block emptyScope
 
 -- XXX should we have different error types depending on where where are in the program?
 data Error
@@ -21,15 +25,47 @@ data Error
   | DuplicateParameter String
   deriving (Show)
 
-readBlock :: BS.ByteString -> Either Error Block
+readBlock :: BS.ByteString -> Either Error (Block 'U)
 readBlock = over _Left ParseError . eitherDecode
 
+type Scope = [String]
+emptyScope = []
+
 -- TODO use Validation instead of Either here
--- TODO use (Source, String) instead of simply `String`, so we know if it was previously declared as a param or a var
--- XXX use `foldM_` here?
-checkUseBeforeDeclare :: Block -> [String] -> Either Error [String]
+-- TODO use mapAccumM to get `Either Error` back
+--      ( https://hackage.haskell.org/package/Cabal-2.4.1.0/docs/src/Distribution.Utils.MapAccum.html )
+resolveTree :: Scope -> Block 'U -> Block 'R
+resolveTree scope (Block lines) = Block $ snd $ mapAccumL resolveLine scope lines
+  where resolveLine :: Scope -> Line 'U -> (Scope, Line 'R)
+        resolveLine scope (LineDecl (Var name)) =
+          -- TODO check shadowing?
+          -- TODO keep nested scopes, so we can check { var a; var a; } vs { var a; {var a;}}?
+          ((name:scope), LineDecl $ Var name)
+
+        resolveLine scope (LineDecl (Fn name params body_)) =
+          let newScope = scope ++ (name:extractParams params) -- TODO check shadowing? for sure check fn name dupes
+          in (newScope, LineDecl $ Fn name params (resolveTree newScope body_))
+
+        resolveLine scope (LineDecl (Import name)) =
+          -- TODO scope should be ([[String]], [String]) to keep namespaces list around, maybe record
+          (undefined:scope, LineDecl $ Import name)
+
+        resolveLine scope (LineExpr _) =
+          (scope, undefined)
+
+        -- TODO steal Either version from checkUseBeforeDeclare
+        extractParams :: ParameterList -> [String]
+        extractParams (ParameterList params) = extractParamName <$> params
+
+        extractParamName :: Parameter -> String
+        extractParamName (Parameter p) = p
+
+        resolveName :: Name 'U -> Name 'R
+        resolveName _ = undefined
+
+checkUseBeforeDeclare :: Block 'U -> [String] -> Either Error [String]
 checkUseBeforeDeclare (Block lines) names = foldM check names lines
-  where check :: [String] -> Line -> Either Error [String]
+  where check :: [String] -> (Line 'U) -> Either Error [String]
         check names (LineDecl (Var name)) = checkDupe name names DuplicateVariable
         -- XXX  we don't check shadowing here, that (++ names possibly has dupes). We can `nub` to remove dupes, or error
         check names (LineDecl (Fn name pr block)) = (extractParamNames pr >>= checkUseBeforeDeclare block . (++ names)) $> names
@@ -53,5 +89,5 @@ rightIf False v _ = Left v
 -- Reformats a block so that it's in this order:
 -- o Variables
 -- o Functions
-reformatBlock :: Block -> Either Error Block
+reformatBlock :: (Block s) -> Either Error (Block s)
 reformatBlock x = Right x
