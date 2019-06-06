91c7fe0556a9e3e4d587c52ca74595a397030a1a
vendethiel <vendethiel@hotmail.fr>
2019-06-06 16:52:11 +0200

    Fix compilation bug by sequencing... with A

--- src/AST.hs
+++ src/AST.hs
@@ -88,18 +88,18 @@ _Fn = prism' (curry3 Fn) $ (\case (Fn s p b) -> Just (s, p, b)
 instance Plated (Decl 'R) where
   plate f (Fn s p b) = Fn s p <$> plateBlock f b
     where plateBlock :: Traversal' (Block s) (Decl s)
-          plateBlock f (Block xs) = Block <$> (plateElem f <*> xs)
+          plateBlock f (Block xs) = Block <$> sequenceA (plateElem f <$> xs)
 
           plateElem :: Traversal' (Line s) (Decl s)
           plateElem f (LineDecl d) = LineDecl <$> f d
           plateElem f (LineExpr e) = LineExpr <$> plateExpr f e
 
           plateExpr :: Traversal' (Expr s) (Decl s)
-          plateExpr f (CallExpr e xs) = CallExpr <$> plateExpr f e <*> (plateExpr f <$> xs)
+          plateExpr f (CallExpr e xs) = CallExpr <$> plateExpr f e <*> sequenceA (plateExpr f <$> xs)
           plateExpr f (LoopExpr e b) = LoopExpr <$> plateExpr f e <*> plateBlock f b
           plateExpr f (ConditionalExpr c t e) = ConditionalExpr <$> plateExpr f c <*> plateBlock f t <*> plateBlock f e
           plateExpr _ e = pure e
-  plate f decl = pure decl
+  plate _ decl = pure decl
 
 instance FromJSON (Decl 'U) where
   parseJSON = withObject "decl" $ \o -> do
