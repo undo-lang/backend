2cda260713711a107e25d492fcc04461dc8c8c0b
vendethiel <vendethiel@hotmail.fr>
2020-09-24 23:26:50 +0200

    why does this infinite loop

--- test/Undo/ASTSpec.hs
+++ test/Undo/ASTSpec.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+
 module Undo.ASTSpec where
 
+import AST
+import Data.Aeson
 import Test.Hspec (describe, it, shouldBe, Spec)
+import Data.ByteString.Lazy
+
+parseBlock :: Data.ByteString.Lazy.ByteString -> Either String (Block 'U)
+parseBlock = eitherDecode
+
+blockShouldBe x y = (parseBlock x) `shouldBe` (Right y)
 
 spec :: Spec
 spec = do
   describe "AST module" $ do
     it "is buildable" $ do
-      1 `shouldBe` 1
+      --"{\"type\": \"Block\"}" `blockShouldBe` Block []
+      (parseBlock "{\"type\": \"Block\",\"body\":[]}") `shouldBe` (Right $ Block [])
